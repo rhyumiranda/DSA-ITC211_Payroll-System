c9f7ff96e5ab0fa1b8bbfec33872d0c97d9aa077
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-10-22 03:04:22 +0800

    feat: create a class for time conversion

--- GroupThree_ProblemThree.java
+++ GroupThree_ProblemThree.java
@@ -35,22 +35,62 @@ public class GroupThree_ProblemThree {
       this.tardinessDeduction = tardinessDeduction;
       this.undertimeDeduction = undertimeDeduction;
     }
-
-    public void print(){
-      System.out.println(employeeName);
-    }
-
-    public static double convertTimeToHour(){
-      return 0;
-    }
-
   }
 
+  public class PayrollSystem {
+    private List<Employee> employees = new LinkedList<>();
+    private Scanner input = new Scanner(System.in);
+  }
 
   public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-
     Employee person1 = new Employee("Omar", 1, "ICS", "CEO", 69, 68, 67, 2, 1.2, 1.5);
-    person1.print();
+  }
+}
+import java.util.*;
+
+public class GroupThree_ProblemThree {
+  public static class Employee{
+    private String employeeName;
+    private double employeeID;
+    private String employeeDepartment;
+    private String employeeRank;
+    private double hours;
+    private double minutes;
+    private double seconds;
+    private int absencesCountDeduction;
+    private double tardinessDeduction;
+    private double undertimeDeduction;
+
+    public Employee(
+            String employeeName,
+            int employeeID,
+            String employeeDepartment,
+            String employeeRank,
+            int hours,
+            int minutes,
+            int seconds,
+            int absencesCountDeduction,
+            double tardinessDeduction,
+            double undertimeDeduction){
+      this.employeeName = employeeName;
+      this.employeeID = employeeID;
+      this.employeeDepartment = employeeDepartment;
+      this.employeeRank = employeeRank;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+      this.absencesCountDeduction = absencesCountDeduction;
+      this.tardinessDeduction = tardinessDeduction;
+      this.undertimeDeduction = undertimeDeduction;
+    }
+  }
+
+  public class PayrollSystem {
+    private List<Employee> employees = new LinkedList<>();
+    private Scanner input = new Scanner(System.in);
+  }
+
+  public static void main(String[] args){
+    Employee person1 = new Employee("Omar", 1, "ICS", "CEO", 69, 68, 67, 2, 1.2, 1.5);
   }
 }
